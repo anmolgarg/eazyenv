6ad3fd4239e3bfdd958f9771620d3b1657d9d550
anmolgarg <anmolgarg314@gmail.com>
2018-03-20 11:40:22 -0700

    update install_req

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -1,13 +1,11 @@
-# update conda things, conda install things
+# update conda things, conda install new things
 conda update --all -y
 conda install plotly psycopg2 -y
 
-# pip install requirements.txt
-pip install -r scripts/requirements.txt
-
-# update pip and pip things
+# update pip things, pip install requirements.txt
 pip install --upgrade pip
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U
+pip install -r scripts/requirements.txt
 
 # create a py2.7 env with numpy
 conda create -n python2 python=2.7 numpy ipykernel -y
